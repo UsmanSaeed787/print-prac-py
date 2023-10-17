c6182897191f30b7ac2f6b966586e06fc2e4b1c6
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-17 19:02:17 +0500

    first commit

---
+++ prac_1.py
@@ -0,0 +1,12 @@
+print("Hello")
+input("")
+
+name = input("What is Your Name ? \n")
+print("Nice to meet You ", name)
+
+ask = input("How are You ? \n")
+
+if ask == "good":
+    print("Great")
+else:
+    print("Development in process!")
